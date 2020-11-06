bbeb8d65b0eab7994e23fe69623509cb8a2f3de4
Yuqi Yun <yuqi.yun@tum.de>
2020-11-06 11:17:52 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+import setuptools
+
+setuptools.setup(
+    name="project",    # Replace with your own username
+    version="0.0.1",
+    description="Denoising & Robust Classification of Sound Signals",
+    url="https://https://gitlab.lrz.de/ml-lab-winter-2020-21/project-1",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
