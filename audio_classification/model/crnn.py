2a8c6b925d02de10ff075c04a2fa6ada396510e4
Yuqi Yun <yuqi.yun@tum.de>
2020-11-21 19:40:29 +0100

    fix import bug

--- audio_classification/model/crnn.py
+++ audio_classification/model/crnn.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from torch.nn.utils.rnn import pack_padded_sequence
+from pytorch_lightning.metrics.functional import accuracy
 from .classifier import Classifier
 
 __all__ = ["LitCRNN"]
