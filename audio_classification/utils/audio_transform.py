e5ea64f4149c5c0d3fc418e42e817038bf22bda7
Yuqi Yun <yuqi.yun@tum.de>
2021-01-11 18:54:54 +0100

    finalize audio rs

--- audio_classification/utils/audio_transform.py
+++ audio_classification/utils/audio_transform.py
@@ -19,3 +19,16 @@ def log_amp_mel_spectrogram(cfg=None):
         T.AmplitudeToDB()
     )
     
+
+# def normalized_mel_spectrogram(cfg=None):
+#     """
+#     Using original sampling_rate, n_fft, hop_length, n_mels from the CRNN paper
+#     """
+#     if cfg['TRANSFORM']['HOP_LENGTH']:
+#         hop_length = cfg['TRANSFORM']['HOP_LENGTH']
+#     else:
+#         hop_length = 256
+#     return nn.Sequential(
+#         T.MelSpectrogram(sample_rate=12000, n_fft=512, hop_length=hop_length, n_mels=96),
+#         T.AmplitudeToDB()
+#     )
